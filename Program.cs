74c961c3e5549dae7bac35226886b346f88a6914
Bysiv01 <45607527+Bysiv01@users.noreply.github.com>
2021-01-17 13:50:21 +0100

    init

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Text;
 
 namespace PIII_Kol3
 {
     class Program
     {
+            string UsuwaczXML(string tekst)
+            {
+            //StringBuilder.Remove(13, 4);
+            return tekst;
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var data = new DateTime(2021,1,17);
+
+            void ObliczSubskrybcje(DateTime data, double wplata = 300)
+            {
+                int iloscMiesiecy = int.Parse(Math.Floor(wplata / 34.99).ToString());
+                double reszta = wplata - (iloscMiesiecy * 34.99);
+                data.AddMonths(iloscMiesiecy);
+                Console.WriteLine(reszta);
+                Console.WriteLine(data);
+            }
+            
+            string text = "jakis tekst <XML>";
+            if (text.Contains("<") & text.Contains(">"))
+            {
+                //UsuwaczXML(text);
+            }
+
         }
     }
 }
